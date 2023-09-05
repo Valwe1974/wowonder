028b26c22679150d84a29b0d87c28b0df5a8f7de
Valwe1974 <94810692+Valwe1974@users.noreply.github.com>
2023-09-05 19:40:01 -0300

    Add files via upload

--- app/mobile/lib/Screens/Memories/MemoriesScreen.dart
+++ app/mobile/lib/Screens/Memories/MemoriesScreen.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wowondertimelineflutterapp/Screens/Chat/NoChatScreen.dart';
+import 'package:wowondertimelineflutterapp/Widget/ScaffoldWidget.dart';
 
+class MemoriesScreen extends StatefulWidget {
+  const MemoriesScreen({super.key});
+
+  @override
+  State<MemoriesScreen> createState() => _MemoriesScreenState();
+}
+
+class _MemoriesScreenState extends State<MemoriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldWidget(
+        AppBarTrueOrFalse: true,
+        title: 'Memories'.tr,
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: NoChatScreen(
+                startChat: false,
+                textData: 'There are no memories',
+              ),
+            )
+          ],
+        ),
+        centerTitle: false,
+        elevation: 0,
+
+        loding: false);
+  }
+}
