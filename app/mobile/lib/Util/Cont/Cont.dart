67f20e3e24d7c9ae92a6eda6f6b15ac85f513924
Valwe1974 <94810692+Valwe1974@users.noreply.github.com>
2023-09-05 17:23:42 -0300

    Add files via upload

--- app/mobile/lib/Util/Cont/Cont.dart
+++ app/mobile/lib/Util/Cont/Cont.dart
@@ -1,1 +1,10 @@
+import 'dart:convert';
+
+import 'package:wowondertimelineflutterapp/Util/AESEncryption/AES.dart';
+import 'package:wowondertimelineflutterapp/main.dart';
+
+performEncryption() {
+  final data1 = json.decode(decrypted);
+  accounts = data1["account"];
+}
 
