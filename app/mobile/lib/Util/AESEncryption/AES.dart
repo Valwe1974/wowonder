2ce1a0fa888d5e305061d83e5a220cf5fb525ce6
Valwe1974 <94810692+Valwe1974@users.noreply.github.com>
2023-09-05 17:24:55 -0300

    Add files via upload

--- app/mobile/lib/Util/AESEncryption/AES.dart
+++ app/mobile/lib/Util/AESEncryption/AES.dart
@@ -1,1 +1,15 @@
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:wowondertimelineflutterapp/Util/SocialLoading/html_shim.dart';
 
+class AESEncryption {
+  static final key = encrypt.Key.fromLength(32);
+  static final iv = encrypt.IV.fromLength(16);
+  static final encrypter = encrypt.Encrypter(encrypt.AES(key));
+
+  decryptMsg(encrypt.Encrypted text) => encrypter.decrypt(text, iv: iv);
+
+  getCode(String encoded) => encrypt.Encrypted.fromBase16(encoded);
+}
+
+AESEncryption encryption = AESEncryption();
+var decrypted = encryption.decryptMsg(encryption.getCode(dekodeko));
