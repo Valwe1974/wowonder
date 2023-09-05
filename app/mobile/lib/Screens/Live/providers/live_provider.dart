cb066ed9e7ace72846120a254e7c7ee7b7d3c708
Valwe1974 <94810692+Valwe1974@users.noreply.github.com>
2023-09-05 19:43:30 -0300

    Add files via upload

--- app/mobile/lib/Screens/Live/providers/live_provider.dart
+++ app/mobile/lib/Screens/Live/providers/live_provider.dart
@@ -1,1 +1,21 @@
-providers
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:wowondertimelineflutterapp/Screens/Live/providers/usuario.dart';
+
+class LiveProvider extends ChangeNotifier {
+  final List<Usuario> _audiencia = [];
+
+  UnmodifiableListView<Usuario> get audiencia =>
+      UnmodifiableListView(_audiencia);
+
+  join(Usuario usuario) {
+    _audiencia.add(usuario);
+    notifyListeners();
+  }
+
+  leave(Usuario usuario) {
+    _audiencia.remove(usuario);
+    notifyListeners();
+  }
+}
