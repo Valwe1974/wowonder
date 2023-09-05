1cff8de463c751a7ae15b0b7e2782c94d8bf65c4
Valwe1974 <94810692+Valwe1974@users.noreply.github.com>
2023-09-05 20:29:29 -0300

    Add files via upload

---
+++ app/mobile/lib/Screens/ChatGpt.dart/widgets/chat_widget.dart
@@ -0,0 +1,104 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wowondertimelineflutterapp/Screens/ChatGpt.dart/services/assets_manager.dart';
+import 'package:wowondertimelineflutterapp/Screens/UserScreen/GetMyUserDataCont.dart';
+import 'package:wowondertimelineflutterapp/ThemesWoWonder.dart';
+
+import 'text_widget.dart';
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget(
+      {super.key,
+      required this.msg,
+      required this.chatIndex,
+      this.shouldAnimate = false});
+
+  final String msg;
+  final int chatIndex;
+  final bool shouldAnimate;
+  @override
+  Widget build(BuildContext context) {
+    var cont = Get.put(GetMyUserDataCont());
+    return Column(
+      children: [
+        Material(
+          color: chatIndex == 0 ? scaffoldBackgroundColorChatGpt : cardColorGpt,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (chatIndex != 0)
+                  Image.asset(
+                    AssetsManager.botImage,
+                    height: 30,
+                    width: 30,
+                  ),
+                if (chatIndex == 0)
+                  CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                      cont.data[0].avatar,
+                    ),
+                  ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: chatIndex == 0
+                      ? TextWidget(
+                          label: msg,
+                        )
+                      : shouldAnimate
+                          ? DefaultTextStyle(
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                              child: AnimatedTextKit(
+                                  isRepeatingAnimation: false,
+                                  repeatForever: false,
+                                  displayFullTextOnTap: true,
+                                  totalRepeatCount: 1,
+                                  animatedTexts: [
+                                    TyperAnimatedText(
+                                      msg.trim(),
+                                    ),
+                                  ]),
+                            )
+                          : Text(
+                              msg.trim(),
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 16),
+                            ),
+                ),
+                chatIndex == 0
+                    ? const SizedBox.shrink()
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(
+                            Icons.thumb_up_alt_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.thumb_down_alt_outlined,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
